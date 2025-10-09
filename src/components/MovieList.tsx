73379372ff7cba247e32647b5a0db8a1012360c7
dimakol <dimakol@gmail.com>
2025-10-09 14:08:59 +0300

    feat: Building the Movie Components
     Adding external libraries - shadcn
     style MovieList and MovieCard components

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,11 +1,14 @@
 import type { Movie, MovieListProps } from "../types";
+import MovieCard from "./MovieCard";
 
 const MovieList = ({ movies }: MovieListProps) => {
   return (
-    <div>
-      <ul>
+    <div className="relative">
+      <ul className="flex overflow-x-scroll overflow-y-visible space-x-4 px-4 md:px-6 py-4 scrollbar-hide relative">
         {movies.map((movie: Movie) => (
-          <li key={movie.id}>{movie.title}</li>
+          <li key={movie.id}>
+            <MovieCard movie={movie} />
+          </li>
         ))}
       </ul>
     </div>
